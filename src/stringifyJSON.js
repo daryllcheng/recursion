537d9cb3aec0b79a9d611da74f170b0bc62b08aa
Daryll <daryllcsh@gmail.com>
2017-01-16 16:04:36 -0800

    Get stringifyJSON.js to pass

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -5,4 +5,32 @@
 
 var stringifyJSON = function(obj) {
   // your code goes here
+	var typeOfObj = typeof obj;
+  if (typeOfObj === "undefined" || typeOfObj === "function") {
+  	return "";
+  }
+  if (obj === null) {
+  	return "null";
+  }
+  if (typeOfObj === "string") {
+	  return '"' + obj + '"';
+  }
+  if (typeOfObj === "boolean" || typeOfObj === "number") {
+	  return String(obj);
+  }
+  if (Array.isArray(obj)) {
+  	return '[' + obj.map(function(element) {
+  		return stringifyJSON(element);
+  	}).join(",") + ']';
+  }
+  if (typeOfObj === "object") {
+  	var stringifiedObj = [];
+  	for (var key in obj) {
+  		if (stringifyJSON(obj[key]) !== "") {
+  			stringifiedObj.push('"' + key + '":' + stringifyJSON(obj[key]));
+  		}
+  	}
+  	return '{' + stringifiedObj.join(",") + '}';
+  }
 };
+
